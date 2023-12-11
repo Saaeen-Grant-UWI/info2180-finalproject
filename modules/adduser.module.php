81ef84347509c9678e8e2161f4d6f82de21be58b
Saaeen-Grant-UWI <saaeengrant.uwi@gmail.com>
2023-12-10 22:59:19 -0500

    Sanitization Alteration

--- modules/adduser.module.php
+++ modules/adduser.module.php
@@ -28,7 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         if(empty($errors)) {
             foreach ($_POST as $key=> $value) {
-                $_POST[$key] = trim($value);
+                $_POST[$key] = strip_tags(trim($value));
             }
             insert("users", [$_POST["firstname"], $_POST["lastname"], password_hash($_POST['password'], PASSWORD_DEFAULT), $_POST["email"], $_POST["role"], date('Y-m-d H:i:s')]);
         }
